5479644e0d4579cd68742dab6f39a3c56e5591bc
lemonlemonde <mirudjun@gmail.com>
2025-02-15 15:46:55 -0800

    :sparkles: init react nextjs r3f
    completely new website :p

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+"use client"; // Ensure it's a Client Component
+
+import { Canvas } from "@react-three/fiber";
+
+export default function Home() {
+  return (
+    <main>
+      <Canvas/>
+    </main>
+  );
+}
