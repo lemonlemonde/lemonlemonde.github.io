ede5ee6a1687de2997abc7c6d29cfd445e44542a
lemonlemonde <mirudjun@gmail.com>
2025-02-15 16:38:06 -0800

    📄 🚧 ghpages shows blank page :(

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+    images: {
+        unoptimized: true, // Disable default image optimization
+    },
+    output: "export",
     transpilePackages: ['three'],
 };
 
