869e69d94443425357a3a0d6ac562f3d2287d192
lemonlemonde <mirudjun@gmail.com>
2025-02-24 01:36:33 -0800

    :construction: More content

--- lemon-website/src/app/page.js
+++ lemon-website/src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 sm:items-start max-w-screen-lg">
         {/* main title */}
         <div>
           <h1 className="text-4xl italic">
@@ -15,8 +15,8 @@ export default function Home() {
 
 
         {/* horizontal blurb */}
-        <div className="flex space-x-20 center w-full">
-          <EyeClosed className="w-1/3" size={100} />
+        <div className="flex space-x-14 center w-full">
+          <EyeClosed className="w-1/3 center" size={100} />
 
           {/* bullet points */}
           <ul className="list-disc space-y-3 font-light w-1/2">
@@ -30,31 +30,70 @@ export default function Home() {
         {/* verticals */}
         <div className="flex w-full space-x-10">
           {/* job */}
-          <ul className="list-disc font-extralight w-1/3">
+          <ul className="list-disc font-extralight w-1/2">
             <h2 className="text-2xl font-normal">
               [ EXP ]
             </h2>
-            <li>i have a job for now</li>
-            <li>probably?</li>
+            <li><b>2024-Current:</b> Data Engineer @ DoD's NSWC Corona</li>
+            <li><b>2024-Current:</b> Student Volunteer @ USC's Virtual Human Therapeutics Lab (VHTL)</li>
+            <li><b>2023-2024:</b> Student Researcher @ USC's Learning and Interactive Robot Autonomy Lab (LiraLab)</li>
+            {/*     
+                • https://www.youtube.com/watch?v=W-ZJ1ijPf8U
+                • https://www.youtube.com/watch?v=8xyuCQlTXEg
+                • https://liralab.usc.edu/comparative-language-feedback/ 
+                */}
+            <li><b>2023-2024:</b> Student Researcher @ USC's Mobile and Environmental Media Lab (MEML)</li>
+            {/* 
+                  • https://storymaps.arcgis.com/collections/1e5f963472f4400da19fcb0e3ccfdd5e?item=1
+                  • https://www.8thwall.com/blog/post/163144919365/meet-the-winners-time-capsule-challenge
+                   */}
+            <li><b>Summer 2023:</b> Intern @ DoD's NSWC Corona</li>
           </ul>
 
           {/* proj links */}
-          <ul className="list-disc font-extralight w-1/3">
+          <ul className="list-disc font-extralight w-1/2">
             <h2 className="text-2xl font-normal">
               [ PROJS ]
             </h2>
-            <li>links to things that can be better</li>
+            <li>Estuary: Augmented Reality Companion Project</li>
+            <li>NASA SUITS Challenge 2022-2023</li>
+            {/* 
+            https://viterbischool.usc.edu/news/2023/06/usc-students-advance-augmented-reality-for-artemis-astronauts/
+             */}
+            <li>Help Is On The Way: USC MFA Thesis VR Film</li>
           </ul>
 
-          {/* publications */}
-          <ul className="list-disc font-extralight w-1/3">
-            <h2 className="text-2xl font-normal">
-              [ PUBS ]
-            </h2>
-            <li>words I don't remember writing</li>
-          </ul>
+
         </div>
 
+        {/* publications */}
+        <ul className="list-disc font-extralight w-fit space-y-1">
+          <h2 className="text-2xl font-normal">
+            [ PUBS ]
+          </h2>
+
+          <li>S. Lin, <b>M. Jun*,</b> B. Rizk*, K. Shieh, S. Fisher, and S. Mozgai, “Optimizing SIA Development: A Case Study in User- Centered Design for Estuary, a Multimodal Socially Interactive Agent Framework,” in ACM Conference on Human Factors in Computing Systems (CHI), 2025.</li>
+
+          <div className="flex-col">
+            <li>S. Lin*, B. Rizk*, <b>M. Jun*,</b> A. Artze, C. Sullivan, S. Mozgai, and S. Fisher, “Estuary: A Framework For Building Multimodal Low-Latency Real-Time Socially Interactive Agents,” in 24th ACM International Conference on Intelligent Virtual Agents (IVA), Dec. 2024.</li>
+            <div className="flex space-x-4">
+              <a className="flex text-slate-500 items-center gap-2 hover:underline hover:underline-offset-4" href="https://arxiv.org/abs/2410.20116">arXiv</a>
+              <a className="flex text-slate-500 items-center gap-2 hover:underline hover:underline-offset-4" href="https://estuary-ai.github.io/ ">Website</a>
+            </div>
+          </div>
+          
+
+          <div className="flex-col">
+            <li>Z. Yang, <b>M. Jun,</b> J. Tien, S. J. Russell, A. Dragan, and E. Bıyık,  “Trajectory Improvement and Reward Learning from Comparative Language Feedback,” in 8th Annual Conference on Robot Learning (CoRL), Sep. 2024.</li>
+            <div className="flex space-x-4">
+              <a className="flex text-slate-500 items-center gap-2 hover:underline hover:underline-offset-4" href="https://arxiv.org/abs/2410.06401v1">arXiv</a>
+              <a className="flex text-slate-500 items-center gap-2 hover:underline hover:underline-offset-4" href="https://liralab.usc.edu/comparative-language-feedback/">Website</a>
+            </div>
+          </div>
+
+          <li>J. Tien*, Z. Yang*, <b>M. Jun,</b> S. J. Russell, A. Dragan, and E. Bıyık, “Optimizing Robot Behavior Via Comparative Language Feedback,” in 3rd Human Robot Interaction (HRI) Workshop on Human-Interactive Robot Learning (HIRL), 2024.</li>
+          <li>M. Potter*, and <b>M. Jun*,</b> “Do Bayesian Neural Networks Improve Weapon System Predictive Maintenance?,” in Annual Reliability and Maintainability Symposium (RAMS), Jan. 2024.</li>
+        </ul>
 
 
       </main>
